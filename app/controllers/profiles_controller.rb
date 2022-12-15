a33d05217293b8b741182ca02a336a68b9a5db57
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-12-14 19:19:01 -0600

    - Added steps to new profile creation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,6 +10,8 @@ class ProfilesController < ApplicationController
   end
 
   def new
+    return redirect_to root_path, alert: 'You already have a profile' if current_user.profile.present?
+
     @profile = Profile.new
   end
 
@@ -19,7 +21,8 @@ class ProfilesController < ApplicationController
 
     if @profile.save
       @profile.profile_state = 'created'
-      redirect_to profile_path(@profile.username), notice: 'Profile Created!'
+      @profile.save
+      redirect_to edit_profile_interests_path(@profile.username), notice: 'Profile created successfully'
     else
       render :new, status: :unprocessable_entity
     end
@@ -29,7 +32,7 @@ class ProfilesController < ApplicationController
     if current_user.profile.username == params[:username].to_s
       @profile = Profile.find_by(username: params[:username])
     else
-      redirect_to root_path, notice: 'You are not allowed to edit this profile'
+      redirect_to root_path, alert: 'You are not allowed to edit this profile'
     end
   end
 
@@ -39,9 +42,13 @@ class ProfilesController < ApplicationController
     if @profile.update(profile_params)
       redirect_to profile_path(@profile.username), notice: 'Profile updated successfully'
     else
-      render :edit, notice: 'Profile not updated', locals: { user: @profile.user, profile: @profile },
+      render :edit, alert: 'Profile not updated', locals: { user: @profile.user, profile: @profile },
                     status: :unprocessable_entity
     end
+  end
+
+  def ubication
+    @profile = Profile.find(params[:id])
   end
 
   def self.titles
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,16 +5,18 @@ class Profile < ApplicationRecord
   has_many :categories, through: :interests
   has_many :events
 
-  validates :username, :first_name, :last_name, :gender, :title, :birthday, presence: true
+  validates :username, :first_name, :last_name, presence: true
   validates :username, length: { in: 3..20 }
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'only allows letters and numbers' }
+  validates :username, uniqueness: true
 
   validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
-  validates :first_name, :last_name, allow_blank: true,
-                                     format: { with: /\A[a-zA-Z]+\z/, message: 'must be a valid name (letters only)' }
+  validates :first_name, :last_name, allow_blank: true, format:
+  { with: /\A[a-zA-Z]+\z/, message: 'must be a valid name (letters only)' }
 
-  validates :gender, inclusion: { in: %w[male female other not_say] }
+  validates :gender, allow_blank: true, inclusion: { in: %w[male female other not_say] }
 
-  validates :title, inclusion: { in: ProfilesController.titles }
+  validates :title, allow_blank: true, inclusion: { in: ProfilesController.titles }
 
   validates :birthday, allow_blank: true, format: { with: /\A[1-2][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])\z/,
                                                     message: 'must be a valid date (yyyy-mm-dd)' }
@@ -28,9 +30,8 @@ class Profile < ApplicationRecord
   validates :phone, allow_blank: true, length: { is: 10 }
   validates :phone, allow_blank: true, numericality: { only_integer: true }
   validates :phone, allow_blank: true, format: { with: /\A[0-9]+\z/, message: 'only allows numbers' }
-  validates :phone, allow_blank: true,
-                    format: { with: /\A[2-9]\d{2}[1-9]\d{2}\d{4}\z/,
-                              message: 'must be a valid phone number, like 555-555-5555' }
+  validates :phone, allow_blank: true, format:
+  { with: /\A[2-9]\d{2}[1-9]\d{2}\d{4}\z/, message: 'must be a valid phone number, like 555-555-5555' }
 
   validates :terms, acceptance: true
 end
